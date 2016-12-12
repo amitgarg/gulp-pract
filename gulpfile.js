94a3718ed9330aaf715d7d2c6a56092f77353fdc
Amit Kumar Garg <akgarg@amadeus.com>
2016-12-12 10:16:02 +0530

    Live reloading with Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('hello', function(){
   console.log('Hello Zell');
@@ -8,12 +9,25 @@ gulp.task('hello', function(){
 gulp.task('sass',function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) //Using gulp-sass
-    .pipe(gulp.dest('app/css'));
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 })
 
 
 
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss',['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
   //Other watchers
+})
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  })
 })
